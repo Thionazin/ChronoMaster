d898f21b7ac3dd9d3d6f04948ab4152740851ad1
Senhe Hao <senhehao@protonmail.com>
2020-08-17 14:19:09 -0500

    It should not have taken this long to add a clock. Also self no longer looks like a word

--- src/main.py
+++ src/main.py
@@ -3,21 +3,32 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.clock import Clock
+import datetime
 
 
 #Creation of the main app class
-class chrono_master(App):
+class borsurth(App):
     def build(self):
         #Layout of the whole thing
-        mainlayout = BoxLayout(orientation = 'vertical')
-        butlayout = BoxLayout(orientation = 'horizontal')
+        main_layout = BoxLayout(orientation = 'vertical')
+        but_layout = BoxLayout(orientation = 'horizontal')
+        upper_layout = BoxLayout(orientation = 'vertical')
 
-        backbutton = Button(text = '<-')
-        forbutton = Button(text = '->')
+        self.countdown_timer = Label(text = 'sample')
 
-        butlayout.add_widget(backbutton)
-        butlayout.add_widget(forbutton)
+        Clock.schedule_interval(self.countdown, 1)
+
+
+        back_button = Button(text = '<-')
+        year_label = Label(text = 'Sample Text')
+        for_button = Button(text = '->')
+
+        but_layout.add_widget(back_button)
+        but_layout.add_widget(year_label)
+        but_layout.add_widget(for_button)
 
         #Layout of calendar portion
         layout = GridLayout(cols=7, rows=5)
@@ -25,10 +36,16 @@ class chrono_master(App):
         for i in range(1, 36):
             layout.add_widget(Button(text = str(i)))
 
-        mainlayout.add_widget(butlayout)
-        mainlayout.add_widget(layout)
+        upper_layout.add_widget(self.countdown_timer)
+        upper_layout.add_widget(but_layout)
+        main_layout.add_widget(upper_layout)
+        main_layout.add_widget(layout)
 
-        return mainlayout
+        return main_layout
+
+    def countdown(self, dt):
+        self.countdown_timer.text = str(datetime.datetime.now())
 
 
-chrono_master().run()
+if __name__ == '__main__': 
+    borsurth().run()
