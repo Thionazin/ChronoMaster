2298d6d240726ddd74fbbc4a517d4e38c2f67cbd
Senhe Hao <senhehao@protonmail.com>
2020-08-16 23:11:08 -0500

    minor stuff added, busy today

--- src/main.py
+++ src/main.py
@@ -2,18 +2,33 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button 
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout 
 
 
 #Creation of the main app class
 class chrono_master(App):
     def build(self):
+        #Layout of the whole thing
+        mainlayout = BoxLayout(orientation = 'vertical')
+        butlayout = BoxLayout(orientation = 'horizontal')
+
+        backbutton = Button(text = '<-')
+        forbutton = Button(text = '->')
+
+        butlayout.add_widget(backbutton)
+        butlayout.add_widget(forbutton)
+
         #Layout of calendar portion
         layout = GridLayout(cols=7, rows=5)
         #Making each button on the layout
         for i in range(1, 36):
             layout.add_widget(Button(text = str(i)))
-        return layout
+
+        mainlayout.add_widget(butlayout)
+        mainlayout.add_widget(layout)
+
+        return mainlayout
 
 
 chrono_master().run()
